70e23fba2a1ce41822937409c19912896a2097a7
Laya mangalagiri <lmangala@uncc.edu>
2023-10-24 13:41:59 -0400

    Initial Commit

---
+++ FuelEaseDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FuelEaseDemo
+//
+//  Created by Laya sagari Mangalagiri on 10/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FuelEaseDemo/FuelEaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelEaseDemoApp.swift
+//  FuelEaseDemo
+//
+//  Created by Laya sagari Mangalagiri on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FuelEaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
