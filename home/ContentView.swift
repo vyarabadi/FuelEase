6a8215f66b0b9443fff30a2059c4fd9b322968d4
Dasha <drizvano@uncc.edu>
2023-10-24 14:44:42 -0400

    Initial Commit

---
+++ home/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  home
+//
+//  Created by Daria Rizvanova on 10/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
