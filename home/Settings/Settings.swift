dec528a8a7d8cc43c4d8446749d72152a086ebcf
Dasha <drizvano@uncc.edu>
2023-10-26 00:20:30 -0400

    completed settings page ( all buttons work and should be ready to link to other pages)

---
+++ home/Settings/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  home
+//
+//  Created by Daria Rizvanova on 10/25/23.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
