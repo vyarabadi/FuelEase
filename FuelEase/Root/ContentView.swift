2ca5c1d9b38622fcc12227679b8aca3525f1c221
kelvin <kelvinnguyen215@gmail.com>
2023-10-27 00:18:33 -0400

    Test

--- FuelEase/Root/ContentView.swift
+++ FuelEase/Root/ContentView.swift
@@ -4,63 +4,53 @@ struct ContentView: View {
     @State private var animateLogo = false
 
     var body: some View {
-        ZStack {
-            Color("bg1")
-                .ignoresSafeArea()
+        NavigationView{
+            ZStack {
+                Color("BackgroundColor")
+                    .ignoresSafeArea()
 
-            VStack {
-                Image("Logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.all)
-                    .frame(width: 300.0, height: 300.0)
-                    .offset(y: animateLogo ? -200 : 0) // Offset animation
-                    .onAppear() {
-                        withAnimation(
-                            Animation.easeInOut(duration: 1.0)
-                                .repeatCount(1, autoreverses: true) // Bounce animation just once
-                        ) {
-                            animateLogo.toggle()
+                VStack {
+                    Image("Logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.all)
+                        .frame(width: 300.0, height: 300.0)
+                        .offset(y: animateLogo ? -20 : 0) // Offset animation
+                        .onAppear() {
+                            withAnimation(
+                                Animation.easeInOut(duration: 1.0)
+                                    .repeatCount(1, autoreverses: true) // Bounce animation just once
+                            ) {
+                                animateLogo.toggle()
+                            }
+                        }
+
+                    HStack {
+                        NavigationLink(destination: Signup()) {
+                            Text("Sign Up")
+                                .foregroundColor(Color("TextColor"))
+                                .font(.custom("AbhayaLibre-Bold", size: 20))
+                                .padding()
+                                .background(Color("Color"))
+                                .cornerRadius(25)
+                        }
+
+                        NavigationLink(destination: Login()) {
+                            Text("Login")
+                                .font(.custom("AbhayaLibre-Bold", size: 20))
+                                .foregroundColor(Color("TextColor"))
+                                .padding()
+                                .background(Color("Color"))
+                                .cornerRadius(25)
                         }
                     }
-
-                HStack {
-                    NavigationLink(destination: Signup()) {
-                        Text("Sign Up")
-                            .foregroundColor(Color("ButtonColor"))
-                            .padding()
-                            .background(Color("bg2"))
-                            .cornerRadius(25)
-                    }
-
-                    NavigationLink(destination: Login()) {
-                        Text("Login")
-                            .foregroundColor(Color("ButtonColor"))
-                            .padding()
-                            .background(Color("bg2"))
-                            .cornerRadius(25)
-                    }
+                    .padding(.top, 20)
                 }
-                .padding(.top, 20)
             }
         }
     }
 }
 
-// Add a Login view (you can create a new view file for Login)
-struct Login: View {
-    var body: some View {
-        Text("Login View")
-    }
-}
-
-@main
-struct FuelEaseDemoApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+#Preview{
+    ContentView()
 }
