dc3c77ae5b27ea61750d7dee0df665176191127f
hroop2 <112510849+hroop2@users.noreply.github.com>
2023-10-27 02:42:27 -0400

    Report_Price

---
+++ FuelEase_Reporing_Price/FuelEase/FuelEaseApp.swift
@@ -0,0 +1,19 @@
+//
+//  FuelEaseApp.swift
+//  FuelEase
+//
+//  Created by Hannah Roop on 10/25/23.
+//
+
+//code that gets executed first
+import SwiftUI
+
+@main
+struct homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
