97f6d73b73436722a4aa67ec828580b952405386
Laya mangalagiri <lmangala@uncc.edu>
2023-10-26 21:26:29 -0400

    Initial Commit

---
+++ FuelEase/FuelEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelEaseApp.swift
+//  FuelEase
+//
+//  Created by Laya sagari Mangalagiri on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FuelEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
